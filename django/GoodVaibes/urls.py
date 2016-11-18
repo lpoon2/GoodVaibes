ee3a4f435ba6273530d7c3a94eb5a5141d116cd1
vpahwa <vpahwa2@illinois.edu>
2016-11-17 23:04:45 -0600

    fix syntax error

--- django/GoodVaibes/urls.py
+++ django/GoodVaibes/urls.py
@@ -29,6 +29,6 @@ router.register('songs', views.SongViewSet)
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^insights/')
 ]
