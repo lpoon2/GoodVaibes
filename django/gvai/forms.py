e34b4987b7aa9dad39700bbd70a639c00a1bdc12
vpahwa <vpahwa2@illinois.edu>
2016-11-18 04:03:06 -0600

    update queries

--- django/gvai/forms.py
+++ django/gvai/forms.py
@@ -6,6 +6,6 @@ form = cgi.FieldStorage()
 searchterm =  form.getvalue('query')
 
 Albums.objects.raw(SELECT * FROM Album WHERE title = %s, searchterm)
-Artists.objects.raw(SELECT * FROM Album WHERE title = %s, searchterm)
-Songs.objects.raw(SELECT * FROM Album WHERE title = %s, searchterm)
+Artists.objects.raw(SELECT * FROM Artists WHERE title = %s, searchterm)
+Songs.objects.raw(SELECT * FROM Songs WHERE title = %s, searchterm)
 
