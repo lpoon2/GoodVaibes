50e26e6aec3fd9b36b2d429663e2a69daac0a58e
vpahwa <vpahwa2@illinois.edu>
2016-11-18 03:56:52 -0600

    SQL queries on get from form

--- django/gvai/forms.py
+++ django/gvai/forms.py
@@ -1,1 +1,11 @@
 from django.forms import ModelForm
+from gvai.models import Albums, Artists, Songs
+import cgi
+
+form = cgi.FieldStorage()
+searchterm =  form.getvalue('query')
+
+Albums.objects.raw(SELECT * FROM Album WHERE title = %s, searchterm)
+Artists.objects.raw(SELECT * FROM Album WHERE title = %s, searchterm)
+Songs.objects.raw(SELECT * FROM Album WHERE title = %s, searchterm)
+
